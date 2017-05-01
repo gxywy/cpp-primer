2711b56e74faa3b426a6747ea37de654f477e5ce
microyu <gxywy@hotmail.com>
2017-05-01 22:12:13 +0800

    update 3.6

---
+++ ch03/3.6/3.6/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using std::cin; using std::cout; using std::endl; using std::string;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	for (auto &n : s)
+		n = 'X';
+	
+	cout << s << endl;
+
+	return 0;
+}
