32f4f4872a6c3652c0c8e9ae69d9cdbc4a8478f4
microyu <gxywy@hotmail.com>
2017-05-01 11:21:57 +0800

    update 3.5

---
+++ ch03/3.5/3.5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using std::cin; using std::cout; using std::endl; using std::string;
+
+int main()
+{
+	string s, total;
+	while (getline(cin, s))
+	{
+		if (s.empty())
+			break;
+		if (total.empty())
+			total += s;
+		else
+			total += " " + s;
+	}
+	cout << total << endl;
+
+	return 0;
+}
