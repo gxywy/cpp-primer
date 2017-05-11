1eed21510b06d478cd03d3ed5e32f7f093e50ee8
microyu <gxywy@hotmail.com>
2017-05-11 18:54:33 +0800

    update:5.11

---
+++ ch03/3.43/3.43/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int ia[3][4] = {
+		{0, 1, 2, 3},
+		{4, 5, 6, 7},
+		{8, 9, 10, 11}
+	};
+
+	for (int (&p)[4] : ia)
+		for (int q : p)
+			cout << q << " ";
+	cout << endl;
+
+	for (int i = 0; i != 3; ++i)
+		for (int j = 0; j != 4; ++j)
+			cout << ia[i][j] << " ";
+	cout << endl;
+
+	for (int(*p)[4] = ia; p != ia + 3; ++p)
+		for (int *q = *p; q != *p + 4; ++q)
+			cout << *q << " ";
+	cout << endl;
+
+	return 0;
+}
